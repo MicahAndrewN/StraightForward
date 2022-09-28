10e3ebeef020d72e13f29eb14bf1400631b88f15
z-dras <zdrasner@umich.edu>
2022-09-28 16:16:57 -0400

    we can talk to the db now woohoo

--- api/src/handler.py
+++ api/src/handler.py
@@ -2,14 +2,16 @@
 import flask # run `pip install Flask` if this is not found
 from flask import Flask
 import json
+import requests
 
 app = Flask(__name__)
 
 # to start server run `python3 /api/src/handler.py` from rootdir
 @app.route("/handler1", methods=["GET"])
 def handle():
-    return json.dumps({'body': 'hi from api'})
-
+    resp = requests.put(url="https://straightforward-89f53-default-rtdb.firebaseio.com/users.json", data=json.dumps({'apiuser': 'API User'}))
+    resp = requests.get(url="https://straightforward-89f53-default-rtdb.firebaseio.com/users.json")
+    return resp.json()
 
 if __name__ == '__main__':
     app.run()
--- screens/CustomizeHome.js
+++ screens/CustomizeHome.js
@@ -5,14 +5,14 @@ import { Text, View } from "../components/Themed";
 import React, { useState, useEffect } from "react";
 
 
-const CustomizeHome = ({navigation}) => {
+const CustomizeHome = ({ navigation }) => {
 
   const [data, setdata] = useState();
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/handler1").then((response) => response.json())
       .then((json) => {
-        setdata(json.body)
+        setdata(json.apiuser)
       }).catch((error) => {
         console.error(error);
         setdata("error parsing json ")
@@ -23,21 +23,21 @@ const CustomizeHome = ({navigation}) => {
       });
   }, []);
   return (
-      <View style={styles.container}>
-        <View>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('Customize',
-              {
-                screen: 'Contacts',
-                params: {
-                }
-              })}
-          >
-            <Text>Add Contact Widget</Text>
-            <Text style={styles.title}>{data}</Text>
-          </TouchableOpacity>
-        </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello user, {data}</Text>
+      <View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Customize',
+            {
+              screen: 'Contacts',
+              params: {
+              }
+            })}
+        >
+          <Text>Add Contact Widget</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 
