33d87ba94c3625babe9b73111c6301b74a392f40
loeher <loeher@umich.edu>
2022-10-28 16:13:00 -0400

    omg it kinda works...

--- screens/Spotify.js
+++ screens/Spotify.js
@@ -1,51 +1,108 @@
-import { StyleSheet, TouchableOpacity } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useContext } from "react";
+import { View, StyleSheet, KeyboardAvoidingView, Text, Button, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+import { ResponseType, useAuthRequest, makeRedirectUri } from "expo-auth-session";
+import { useSelector, useDispatch } from "react-redux";
+import * as tokenAction from "../store/actions/token";
+import axios from "axios";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AuthContext from "../components/AuthContext";
 
-import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+const discovery = {
+  authorizationEndpoint: "https://accounts.spotify.com/authorize",
+  tokenEndpoint: "https://accounts.spotify.com/api/token",
+};
 
-// import {
-//     auth as SpotifyAuth,
-//     remote as SpotifyRemote,
-//     ApiScope,
-//     ApiConfig,
-//   } from "react-native-spotify-remote";
+const Spotify = ({ navigation }) => {
+  // const dispatch = useDispatch();
+  const [token, setToken] = useState("");
+  const redirectUri = makeRedirectUri();
+  console.log(redirectUri);
+  const auth = useContext(AuthContext)
 
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      responseType: ResponseType.Token,
+      clientId: "e36f952ff0df46de96031217f169216a",
+      scopes: [
+        "user-read-currently-playing",
+        "user-read-recently-played",
+        "user-read-playback-state",
+        "user-top-read",
+        "user-modify-playback-state",
+        "streaming",
+        "user-read-email",
+        "user-read-private",
+      ],
+      // In order to follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
+      // this must be set to false
+      usePKCE: false,
+      redirectUri: redirectUri,
+    },
+    discovery
+  );
 
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { access_token } = response.params;
+      setToken(access_token);
+    }
+  }, [response]);
 
-const Spotify = ({navigation}) => {
+  useEffect(() => {
+    if (token) {
+      axios("https://api.spotify.com/v1/me/top/tracks?time_range=short_term", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      })
+        .then((response) => {
+          dispatch(songAction.addTopSongs(response));
+        })
+        .catch((error) => {
+          console.log("error", error.message);
+        });
 
-    // const [session, setSession] = useState(null)
-
-    // const spotifyConfig = ApiConfig = {
-    //     clientID: "e36f952ff0df46de96031217f169216a",
-    //     redirectURL: "www.google.com", // change
-    //     tokenRefreshURL: "SPOTIFY_TOKEN_REFRESH_URL",
-    //     tokenSwapURL: "SPOTIFY_TOKEN_SWAP_URL",
-    //     scopes: [ApiScope.AppRemoteControlScope, ApiScope.UserFollowReadScope],
-    //   };
-
-    //   async function logInUser(){
-    //     try{
-    //         const token = await SpotifyAuth.authorize(spotifyConfig);
-    //         await SpotifyRemote.connect(token);
-    //         setSession(SpotifyRemote.connect(token));
-    //     }catch(err){
-    //         console.error("Couldn't authorize with or connect to Spotify",err);
-    //     }
-    // }
-
-    return (
-        <View style={styles.container}>
-          <TouchableOpacity
-          style={styles.drive}
-          onPress={() => console.log("pressed")}
-        >
-          <Text style={styles.driveText}>Connect Your Spotify Account</Text>
-        </TouchableOpacity>
-        </View>
+      setTimeout(
+        () =>
+          navigation.replace("Home", {
+            token: token,
+            other: "blaaaa",
+          }),
+        500
       );
 
-}
+      // dispatch(tokenAction.addToken(token));
+    }
+  });
+
+  return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <StatusBar style="light" />
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            color: "white",
+            marginBottom: "20%",
+          }}
+        >
+        </Text>
+        <Button
+          title="Login with Spotify"
+          style={styles.button}
+          onPress={() => {
+            promptAsync();
+          }}
+        />
+        <View style={{ height: 100 }} />
+      </KeyboardAvoidingView>
+  );
+};
 
 export default Spotify;
 
@@ -54,30 +111,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "white",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-  drive: {
-    alignItems: 'center',
-    backgroundColor: 'green',
-    marginTop: 10,
-    marginBottom: 10,
-    height: 65,
-    width: 250,
-    borderRadius: 20,
-    fontWeight: 'bold'
-  },
-  driveText: {
-    fontSize: 24,
-    marginTop: 15,
-    color: '#FFFFFF',
-    fontWeight: 'bold'
+
+  button: {
+    width: 200,
+    marginTop: 50,
   },
 });
